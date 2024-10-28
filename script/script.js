a27a9a0c03c6fb5281ba6402e0652f861f63e664
a22iagopp <a22iagopp@iessanclemente.net>
2024-10-28 13:53:52 +0100

    "Fixed iframe resizing function problem"

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
-function changePage(pageName, origin) {
+function changePage(pageName) {
 
-    var iframeSRC = parent.document.getElementById("iframe");
+    var iframeSRC = parent.document.getElementById("myIframe");
 
-    iframeSRC.src=pageName;
-
-    
+    iframeSRC.src = pageName;
 }
+
+function sendHeightToParent() {
+    const height = document.body.scrollHeight;
+    window.parent.postMessage({ type: 'resizeIframe', height: height }, '*');
+  }
+
+// Send height on load and whenever content changes if necessary
+window.onload = sendHeightToParent;
+// Optionally, call sendHeightToParent() on specific events, like form inputs, button
